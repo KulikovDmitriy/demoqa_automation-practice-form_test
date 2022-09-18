fac9891205853b4fbdc3ee6a0ea7e57c5f3cce2a
kulikovdmitriy <kulikch@gmail.com>
2022-09-18 17:19:15 +0300

    update build.gradle - added selenide

---
+++ src/test/java/com/demoqa/RegistrationFormTests.java
@@ -0,0 +1,21 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class RegistrationFormTests {
+
+    @BeforeAll
+    static void
+
+
+    @AfterAll
+    static void tearDown() {
+
+    }
+
+
+    @Test
+
+}
